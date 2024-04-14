e334f12598898f0778c6c1dcd2329e8f8659b593
s28691 <s28691@pjwstk.edu.pl>
2024-04-15 00:38:55 +0200

    Adding Animal and Visit class

---
+++ apbd19-cwiczenia5/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace apbd19_cwiczenia5;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public int Age { get; set; }
+    public string Color { get; set; }
+}
---
+++ apbd19-cwiczenia5/Visit.cs
@@ -0,0 +1,10 @@
+﻿namespace apbd19_cwiczenia5;
+
+public class Visit
+{
+    public int Id { get; set; }
+    public DateTime VisitDate { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int AnimalId { get; set; }
+}
